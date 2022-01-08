44464ee9f9072a609cf4c8eb930abfce53026b3c
verydianT <verydian.t@gmail.com>
2022-01-09 00:18:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard1 from "./page/peserta/dashboard";
+import Profile from "./page/peserta/profile";
+import A_Dashboard from "./page/admin/dashboard";
+import M_Dashboard from "./page/mentor/dashboard";
+import M_detail from "./page/mentor/detail-pekerjaan";
+import Data_M from "./page/admin/data_mentor";
+import Data_P from "./page/admin/data_peserta";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Dashboard1/> */}
+      {/* <Profile/> */}
+      {/* <A_Dashboard/> */}
+      {/* <M_Dashboard/> */}
+      {/* <M_detail/>  */}
+      {/* <Data_M/> */}
+      <Data_P/>
     </div>
   );
 }
